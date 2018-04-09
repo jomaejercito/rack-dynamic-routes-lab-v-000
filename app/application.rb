1b589d8eebe8292d5c0f205e35048a050517690c
Joma Ejercito <jomaejercito@gmail.com>
2018-04-09 20:08:27 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -8,15 +8,12 @@ class Application
 
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
-      item = Item.item.find{|i| i.name == item_name}
-
-      if item == nil
-        resp.write "Item not found"
-        resp.status = 400
+      if item = @@items.find { |i| i.name == item_name}
+          resp.write item.price
       else
-        resp.write item.price
+          resp.write "Item not found"
+          resp.status = 400
       end
-
     else
       resp.write "Route not found"
       resp.status = 404
